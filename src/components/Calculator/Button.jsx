172059ca6dec65032b2a29cc2620f85b8800c327
Peregon Bogdan <peregonbw@gmail.com>
2020-02-16 10:46:48 +0200

    New calculator

---
+++ src/components/Calculator/Button.jsx
@@ -0,0 +1,23 @@
+import React, {useRef} from "react";
+
+export const Button = props => {
+    const ref = useRef();
+    const buttonClick = () => {
+        let classList = ref.current.classList;
+        classList.add("active");
+        setTimeout(() => {
+            classList.remove("active");
+        }, 100);
+    };
+
+    return (
+        <div ref={ref}
+             className={`button${props.classList ? " " + props.classList : ""}`}
+             onClick={() => {
+                 buttonClick();
+                 props.onClick();
+             }}>
+            {props.value}
+        </div>
+    )
+};
---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,156 @@
+import React, {useState} from "react";
+import backspace from "../../assets/img/backspase.svg";
+import {Button} from "./Button";
+import * as math from 'mathjs';
+
+export const Calculator = () => {
+
+    let [inputValue, setInputValue] = useState("");
+    let [outputValue, setOutputValue] = useState("");
+    let [historyValue, setHistoryValue] = useState("");
+    let [operator, setOperator] = useState("");
+    let [error, setError] = useState("");
+
+    const insert = (arr, index, newItem) => [
+        ...arr.slice(0, index),
+        newItem,
+        ...arr.slice(index)
+    ];
+
+    const numberClick = (num) => {
+        if (!(inputValue === "000" && num === "0")) {
+            setError("");
+            let numbers;
+            numbers = inputValue.match(/[:]/) ? (inputValue.split(/[:]/)[0].concat(inputValue.split(/[:]/)[1])) + num : inputValue + num;
+            let transform = (num) => parseInt(num).toString(),
+                numbersTransformed = transform(numbers);
+
+            if (numbersTransformed.length < 9) {
+                setInputValue(numbers);
+                let arrNumbers;
+                arrNumbers = () => {
+                    if (numbersTransformed.length === 1) {
+                        return "00" + numbersTransformed;
+                    } else if (numbersTransformed.length === 2) {
+                        return "0" + numbersTransformed;
+                    } else if (numbersTransformed.length >= 3) {
+                        return numbersTransformed;
+                    }
+                };
+                let arr1 = Array.from(arrNumbers());
+                setOutputValue(insert(arr1, arr1.length - 2, ":").join(''));
+            } else {
+                setError("Максимально 8 значений")
+            }
+        }
+    };
+
+    const clear = () => {
+        setInputValue("");
+        setOutputValue("");
+        setError("");
+    };
+
+    const backspaceClick = () => {
+        let spliced = outputValue.split(/[:]/)[0].concat(outputValue.split(/[:]/)[1]).slice(),
+            result,
+            length = spliced.length;
+
+        if (outputValue.length > 4) {
+            result = spliced.slice(0, length - 3) + ":" + spliced.slice(length - 3, length - 1);
+        } else if (outputValue.length === 4) {
+            result = "0:" + spliced.slice(length - 3, length - 1);
+        } else if (outputValue.length === 3) {
+            result = "0:" + spliced.slice(length - 2, length - 1);
+        } else if (outputValue.length <= 2) {
+            result = ""
+        }
+        setOutputValue(result);
+        setInputValue(result);
+    };
+
+    const operatorClick = (op) => {
+        setOperator(op);
+        setHistoryValue(outputValue);
+        setInputValue("");
+    };
+
+    const equalClick = () => {
+        if (operator !== "") {
+            let reserve = outputValue;
+            let convertToMinutes = (num) => {
+                    let hours = num.split(/[:]/)[0];
+                    let minutes = num.split(/[:]/)[1];
+                    return parseInt(math.evaluate((parseInt(hours) * 60) + parseInt(minutes)));
+                },
+                minutesResult = (num1, operator, num2) => {
+                    return math.evaluate(parseInt(num1) + operator + parseInt(num2));
+                },
+                convertToExpression = (num) => {
+                    let hours = Math.floor(num / 60);
+                    let minutes = num - hours * 60;
+                    return hours + ":" + minutes;
+                },
+                result = convertToExpression(minutesResult(convertToMinutes(historyValue), operator, convertToMinutes(outputValue)));
+            setOutputValue(result);
+            console.log(outputValue, reserve)
+        }
+    };
+
+    return (
+        <div className="App">
+            <span className="errorMessage">{error}</span>
+            <input readOnly={true} placeholder={"0:00"} className="input big" value={outputValue}/>
+            <div className="section">
+                <Button value="7" onClick={() => {
+                    numberClick("7")
+                }}/>
+                <Button value="8" onClick={() => {
+                    numberClick("8")
+                }}/>
+                <Button value="9" onClick={() => {
+                    numberClick("9")
+                }}/>
+                <Button value={<img src={backspace} alt={"backspace"}/>} onClick={() => {
+                    backspaceClick();
+                }}/>
+                <Button value="4" onClick={() => {
+                    numberClick("4")
+                }}/>
+                <Button value="5" onClick={() => {
+                    numberClick("5")
+                }}/>
+                <Button value="6" onClick={() => {
+                    numberClick("6")
+                }}/>
+                <Button value="-" onClick={() => {
+                    operatorClick("-")
+                }}/>
+                <Button value="1" onClick={() => {
+                    numberClick("1")
+                }}/>
+                <Button value="2" onClick={() => {
+                    numberClick("2")
+                }}/>
+                <Button value="3" onClick={() => {
+                    numberClick("3")
+                }}/>
+                <Button value="+" onClick={() => {
+                    operatorClick("+")
+                }}/>
+                <Button value="0" classList="double" onClick={() => {
+                    numberClick("0")
+                }}/>
+                <Button value="C" onClick={() => {
+                    clear();
+                }}/>
+                <Button value="=" onClick={() => {
+                    equalClick();
+                }}/>
+            </div>
+        </div>
+    )
+};
+
+// TODO BACKSPACE WHEN OPERATOR
+// TODO ADD ZEROS ON OPERATOR
