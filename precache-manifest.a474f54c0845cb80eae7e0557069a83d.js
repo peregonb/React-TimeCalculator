0d66c7c7f784296a9eade9de30d24170531989d1
Peregon Bogdan <peregonbw@gmail.com>
2020-02-17 15:53:36 +0200

    Updates

---
+++ precache-manifest.a474f54c0845cb80eae7e0557069a83d.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b803e7474a10f4168e3247f0c84ec7cd",
+    "url": "/React-TimeCalculator/index.html"
+  },
+  {
+    "revision": "05aef932fba67ad9c1f6",
+    "url": "/React-TimeCalculator/static/css/main.3b0aa107.chunk.css"
+  },
+  {
+    "revision": "15037cda541887f3ee2b",
+    "url": "/React-TimeCalculator/static/js/2.bbe15244.chunk.js"
+  },
+  {
+    "revision": "05aef932fba67ad9c1f6",
+    "url": "/React-TimeCalculator/static/js/main.754de954.chunk.js"
+  },
+  {
+    "revision": "30ab696d7f575ddf632f",
+    "url": "/React-TimeCalculator/static/js/runtime-main.59f9c9eb.js"
+  },
+  {
+    "revision": "8249902c73b4219f19776974aa11706e",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Black.8249902c.woff"
+  },
+  {
+    "revision": "9009277f1d98ee2d48e1bcdc69a2da22",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Black.9009277f.ttf"
+  },
+  {
+    "revision": "9b191b2e013834ab2546cdfd1953a85e",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Black.9b191b2e.eot"
+  },
+  {
+    "revision": "b942a2a2df4a9a1e2a4dc023a46e3f96",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Black.b942a2a2.woff2"
+  },
+  {
+    "revision": "0c12ee2fd868459a1db633259b2ba50a",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Bold.0c12ee2f.ttf"
+  },
+  {
+    "revision": "242780083d90e494fa8a5f41a6a4d2cd",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Bold.24278008.woff"
+  },
+  {
+    "revision": "35b1efc06970f9de3ea323ab868133a1",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Bold.35b1efc0.woff2"
+  },
+  {
+    "revision": "42198212b25644482557cfb156f4893e",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Bold.42198212.eot"
+  },
+  {
+    "revision": "147dc989e55f31dd4af80149816177ea",
+    "url": "/React-TimeCalculator/static/media/Montserrat-ExtraBold.147dc989.ttf"
+  },
+  {
+    "revision": "793acf44892dc1bed99a059ad418bac6",
+    "url": "/React-TimeCalculator/static/media/Montserrat-ExtraBold.793acf44.woff2"
+  },
+  {
+    "revision": "b6d83d4c5b11e93151fcc134dff1dec3",
+    "url": "/React-TimeCalculator/static/media/Montserrat-ExtraBold.b6d83d4c.woff"
+  },
+  {
+    "revision": "c526f45d260140c09bd298bb6e9d6f38",
+    "url": "/React-TimeCalculator/static/media/Montserrat-ExtraBold.c526f45d.eot"
+  },
+  {
+    "revision": "713396da25af87cf6860728f0b4b9210",
+    "url": "/React-TimeCalculator/static/media/Montserrat-ExtraLight.713396da.woff"
+  },
+  {
+    "revision": "7b88fd873b09d2b2f6b211ecd52ed05e",
+    "url": "/React-TimeCalculator/static/media/Montserrat-ExtraLight.7b88fd87.eot"
+  },
+  {
+    "revision": "cd9bac48325d83b76bd375549cf1a90b",
+    "url": "/React-TimeCalculator/static/media/Montserrat-ExtraLight.cd9bac48.woff2"
+  },
+  {
+    "revision": "df1a23dffa40405703ab1440d410775e",
+    "url": "/React-TimeCalculator/static/media/Montserrat-ExtraLight.df1a23df.ttf"
+  },
+  {
+    "revision": "0ff9c906b0e48a4fd91821b6d5a89b09",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Light.0ff9c906.eot"
+  },
+  {
+    "revision": "4621f900af0a8edb59b6d2caf2c7d653",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Light.4621f900.woff2"
+  },
+  {
+    "revision": "57a8d90fbe52cdc657be8d679f925066",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Light.57a8d90f.ttf"
+  },
+  {
+    "revision": "7022f635003746c50240a31532dd57c5",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Light.7022f635.woff"
+  },
+  {
+    "revision": "3274651502e8c7a4755f07d7cc838cac",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Medium.32746515.eot"
+  },
+  {
+    "revision": "3cf57b2e4b30b1e316f171b5366b332a",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Medium.3cf57b2e.woff2"
+  },
+  {
+    "revision": "5a29fda9494fd8b9f8731413585f5eb6",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Medium.5a29fda9.woff"
+  },
+  {
+    "revision": "7f2231ac2105d960197a62a2db1cf0e4",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Medium.7f2231ac.ttf"
+  },
+  {
+    "revision": "02857e350dfdeb82a2add68d835fee79",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Regular.02857e35.eot"
+  },
+  {
+    "revision": "0eb00d2a4db71c28ad121fa98a571881",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Regular.0eb00d2a.woff"
+  },
+  {
+    "revision": "23488bf2168305ed74a3b4510664894a",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Regular.23488bf2.woff2"
+  },
+  {
+    "revision": "6890001898a6fc1b36ddc2ed0195e2d8",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Regular.68900018.ttf"
+  },
+  {
+    "revision": "03f2ccdd2edf5579d5a6cf365cc11d93",
+    "url": "/React-TimeCalculator/static/media/Montserrat-SemiBold.03f2ccdd.ttf"
+  },
+  {
+    "revision": "4b9180932e60a1508fef4a162650d7e5",
+    "url": "/React-TimeCalculator/static/media/Montserrat-SemiBold.4b918093.woff"
+  },
+  {
+    "revision": "c512a78eb2abd03461456fbbedcfeb93",
+    "url": "/React-TimeCalculator/static/media/Montserrat-SemiBold.c512a78e.eot"
+  },
+  {
+    "revision": "e62c69a590f83d3122e256a19eac0e8b",
+    "url": "/React-TimeCalculator/static/media/Montserrat-SemiBold.e62c69a5.woff2"
+  },
+  {
+    "revision": "0b4817f14eba116a249c01e2381e22fa",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Thin.0b4817f1.woff"
+  },
+  {
+    "revision": "1ff186b581b2cc4648f92e9194a01d24",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Thin.1ff186b5.woff2"
+  },
+  {
+    "revision": "ba1f065d3beb89f8c0adffc3f4247847",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Thin.ba1f065d.ttf"
+  },
+  {
+    "revision": "d298af214c890fa276a2c96c71431ba5",
+    "url": "/React-TimeCalculator/static/media/Montserrat-Thin.d298af21.eot"
+  },
+  {
+    "revision": "17f8a12e6482843685f0dfe74c237bbb",
+    "url": "/React-TimeCalculator/static/media/backspase.17f8a12e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-TimeCalculator/precache-manifest.e2864243e811c76a389a853b12f910b6.js"
+  "/React-TimeCalculator/precache-manifest.a474f54c0845cb80eae7e0557069a83d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-TimeCalculator/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
